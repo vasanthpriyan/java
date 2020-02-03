e6159eae05ae122b608f7d2ff806b979e341b390
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-03 23:09:04 +0530

    Create Q1-Series I.java

---
+++ Control Structures II/Q1-Series I.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main
+{
+	public static void main(String args[])
+	{
+		Scanner vasanth=new Scanner(System.in);
+      int a=vasanth.nextInt();
+      for (int i=1;i<a+1;i++){
+      int b=i*i;
+        System.out.printf("%d ",b);
+      }
+	}
+}
