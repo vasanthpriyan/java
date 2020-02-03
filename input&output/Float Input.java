f4a01c19fb4d5495caddbca1c71c680be7f58646
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-03 22:30:45 +0530

    Create Float Input.java

---
+++ input&output/Float Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        float a=in.nextFloat();
+          System.out.println(a);
+        
+    }
+}
