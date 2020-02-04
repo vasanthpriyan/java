c7a76640bb45362f2f9623ba046569f1f322ee40
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-04 10:39:25 +0530

    Update Q5-Magic Square.java

--- 2D-ARRAY/Q5-Magic Square.java
+++ 2D-ARRAY/Q5-Magic Square.java
@@ -3,26 +3,73 @@ class Main
 {
 public static void main(String args[])
 {
-Scanner ob=new Scanner(System.in);
-        int a=ob.nextInt();
-      int c[][]=new int[50][50];
-      int i,j;
-      for(i=0;i<a;i++)
+ Scanner aa=new Scanner(System.in);
+      int m=aa.nextInt();
+      int a[][]=new int[m][m];
+      int result[]=new int[m];
+       int result1[]=new int[m];
+      int dsum=0;
+        int d1sum=0;
+      int z=0;
+     
+      for(int i=0;i<m;i++)
       {
-       for(j=0;j<a;j++)
-       {
-         c[i][j]=ob.nextInt();
+         for(int j=0;j<m;j++)
+         {
+         a[i][j]=aa.nextInt();
+         }
+      }
+     
+       int max=0;
+      for(int i=0;i<m;i++)
+      {
+         for(int j=0;j<m;j++)
+         {
+          max=max+a[i][j];
+           }
+          result[i] = max;
+          max=0;
+      }
+      int max1=0;
+      for(int j=0;j<m;j++)
+      {
+         for(int i=0;i<m;i++)
+        {
+          max1=max1+a[i][j];
+           }
+          result1[j] = max1;
+          max1=0;
+      }
+      for(int i=0;i<m;i++)
+      {
+      dsum+=a[i][i];
+      d1sum+=a[i][m-1-i];
+      }
+     
+      for(int i=0;i<result.length;i++)
+      {
+     
+      if(result[i]==result1[i])
+      {
+     
+        z=result[0];
        }
-      }
+       else
+       {
        
+       z=result1[0];
+       }
      
-     
-     if(a==3)
-      {      
-         System.out.println("yes");
       }
-          else
-          {
-        System.out.println("no");
+     
+      if((z==dsum)&&(z==d1sum))
+         {
+         System.out.println("yes");
+         }
+         else
+         {
+         
+         System.out.println("no");
+         }
+         }
 }
-}}
