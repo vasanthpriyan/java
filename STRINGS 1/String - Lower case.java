db356f8aa2083636af3161be55ce13e0f473a65d
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-06 11:41:18 +0530

    Create String - Lower case.java

---
+++ STRINGS 1/String - Lower case.java
@@ -0,0 +1,11 @@
+ import java.util.Scanner;
+class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+      String a=sc.nextLine();
+    String b=a.toLowerCase();
+       System.out.println(b);
+      }
+}
