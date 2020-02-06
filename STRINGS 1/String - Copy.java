cd92319ef0a6b27eddf2128793e3135593c3f55a
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-06 11:54:34 +0530

    Create String - Copy.java

---
+++ STRINGS 1/String - Copy.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+      String a=sc.nextLine();
+     String b=String.valueOf(a);
+     
+      System.out.println(b);
+}
+}
