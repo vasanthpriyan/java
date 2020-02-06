57e42168f91c859840daee3854e00d7ba06b9fe6
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-06 11:50:18 +0530

    Create String - Concatenate.java

---
+++ STRINGS 1/String - Concatenate.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+      String a=sc.nextLine();
+      String b=sc.nextLine();
+      String c=a.concat(b);
+      System.out.println(c);
+}
+}
