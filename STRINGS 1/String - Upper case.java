c54f5aca443f90026e3ba22f6a97c2be20b33b4c
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-06 11:39:57 +0530

    Create String - Upper case.java

---
+++ STRINGS 1/String - Upper case.java
@@ -0,0 +1,11 @@
+ import java.util.Scanner;
+class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+      String a=sc.nextLine();
+    String b=a.toUpperCase();
+       System.out.println(b);
+      }
+}
