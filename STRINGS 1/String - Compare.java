5c9f2f94a690fcfa5660606509df1511b44fbade
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-06 11:38:31 +0530

    Create String - Compare.java

---
+++ STRINGS 1/String - Compare.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+      String a=sc.nextLine();
+      String b=sc.nextLine();
+     if(a.equals(b))
+      {
+      System.out.println("Strings are same");
+      }
+      else
+      {
+       System.out.println("Strings are not same");
+      }
+     
+}
+}
