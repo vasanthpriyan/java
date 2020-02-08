66f18dd57b91e2635e68b256c0bdba292ca0d65a
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-08 10:19:29 +0530

    Create Area calculation.java

---
+++ operators/Area calculation.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+      Scanner s=new Scanner(System.in);
+      int side=s.nextInt();
+      int rectangle_length=s.nextInt();
+       int rectangle_breadth=s.nextInt();
+       float r=s.nextFloat();
+              System.out.println(side*side);
+    
+      System.out.println( rectangle_length* rectangle_breadth);
+       System.out.println(3.14*r*r);
+      
+    }
+}
