1f4fe9db03fc999a9a07737ccc99921bfd5b8df8
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-08 10:18:04 +0530

    Create average calculation.java

---
+++ operators/average calculation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+      float a=s.nextFloat();
+       float b=s.nextFloat();
+       float c=s.nextFloat();
+       float d=s.nextFloat();
+       float e=s.nextFloat();
+      System.out.println((a+b+c+d+e)/5);
+      
+    }
+}
