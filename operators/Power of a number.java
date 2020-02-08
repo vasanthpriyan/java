8a228d57222294d66568c1c9e2c6c58d848aec1f
vasanthpriyan <60595497+vasanthpriyan@users.noreply.github.com>
2020-02-08 10:21:25 +0530

    Create Power of a number.java

---
+++ operators/Power of a number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+       Scanner s=new Scanner(System.in);
+   int x=s.nextInt();
+int n=s.nextInt();
+System.out.println(Math.pow(x,n));
+  
+    }
+}
